dcefd0d3bd72b120772239f8cef7cf4f7b7f0536
paige <paigetetz@gmail.com>
2022-09-06 16:52:56 -0400

    commit tree 15cf88ce63c1e6242629c6876b2aae8efbddda90

---
+++ index.js
@@ -0,0 +1,26 @@
+function sayHiToHeadphonedRoomate() {
+  console.log("Hello!");
+}
+function sayHiToHeadphonedRoomate(){
+  if (string === string.toLowerCase){
+  return shout("I can't hear you!")
+  if (string === string.toUpperCase)
+  {return shout ("YES INDEED")}
+  else if (string === "Let's have dinner together!")
+  {return ("I would love to!")
+  }
+}
+function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+function logShout(string) {
+    console.log(string.toUpperCase())
+  }
+  function logWhisper(string) {
+    console.log(string.toLowerCase())
+  }
+}
